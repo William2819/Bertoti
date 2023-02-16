cb373c2b42a12361996491bd6c52c6c594ca96cb
William Claudimar <93256317+William2819@users.noreply.github.com>
2023-02-16 17:52:33 -0300

    Add files via upload

---
+++ Engenharia III/src/Atacante.java
@@ -0,0 +1,8 @@
+public class Atacante implements Futebol{
+    @Override
+    public void chutarBola() {
+        System.out.println("O atacante ou avançado é uma posição de linha do futebol, 
+        que desempenha função ofensiva, com objetivo de fazer gols ou dar assistências à seus companheiros 
+        em ataques impulsivos contra o time adversário.");
+    }
+}
---
+++ Engenharia III/src/Futebol.java
@@ -0,0 +1,3 @@
+public interface Futebol {
+    public void chutarBola();
+}
