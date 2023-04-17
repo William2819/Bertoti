be2051bd9bad9ccd5d74188b6ac4edc9f6a8f536
William Claudimar <93256317+William2819@users.noreply.github.com>
2023-04-17 09:51:45 -0300

    Add files via upload

---
+++ Engenharia III/Strategy I/src/Esporte.java
@@ -0,0 +1,13 @@
+public class Esporte {
+    private Futebol futebol;
+    public void setFutebol(Futebol sfutebol){
+        futebol = sfutebol;
+    }
+
+    public void jogarFutebol(){
+        futebol.chutarBola();
+
+    }
+
+
+}
